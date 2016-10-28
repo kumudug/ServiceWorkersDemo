163ce940bbd59a4502556dfd66fe1d879ceba4f1
kumudug <kumudug1@gmail.com>
2016-10-28 20:56:56 +1100

    initial

---
+++ app/register.js
@@ -0,0 +1,60 @@
+﻿"use strict";
+
+(function () {
+
+    if (!navigator.serviceWorker.controller) {
+        console.log("first time serviceworker is loaded and running...");
+        //return;
+    }else {
+        console.log("A version of service worker is loaded and running...");
+    }
+
+    function updateConfirmation(sw) {
+        if (confirm("There is an updated version available. Install now ?")) {
+            sw.postMessage({ action: "installUpdate" });
+        }
+    }
+
+    function trackInstalation(sw) {
+        if (sw.addEventListner) {
+            sw.addEventListner("statechange", () => {
+                if (sw.state === "installed") {
+                    updateConfirmation(sw);
+                }
+            });
+        }
+    }
+
+    if ('serviceWorker' in navigator) {
+
+        console.log('service worker api is present');
+
+        navigator.serviceWorker.register('/sw.js').then(function (reg) {
+
+            if (reg.installing) {
+                console.log('New versioni installing...');
+                trackInstalation(reg.installing);
+            } else if (reg.waiting) {
+                console.log('New version available to install...');
+                updateConfirmation(reg.waiting);
+            } else if (reg.active) {
+                console.log('Service worker active');
+            }
+
+        }).catch(function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+
+    } else {
+        console.log('service worker api is not present in the browser');
+    }
+
+})();
+
+
+
+
+
+
+
+
---
+++ sw.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var CACHE_NAME = 'sv-cache-v19';
+
+self.addEventListener("install", (ev) => {
+    // Perform install steps
+    ev.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('Opened cache');
+            //return cache.addAll(urlsToCache);
+        }));
+});
+
+self.addEventListener("activate", (ev) => {
+
+    ev.waitUntil(caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map(function (key) {
+                if (CACHE_NAME.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+});
